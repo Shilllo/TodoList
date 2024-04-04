a5ad294cc5bbd841afbd840f84e8d8a509d543f2
shillo <kimsemen0402@mail.ru>
2024-04-04 18:13:45 +0300

    eventController.js: fixed bug with multiple forms for new project

--- src/eventController.js
+++ src/eventController.js
@@ -2,33 +2,35 @@ import { addProject } from './Project'
 
 function startEvents(instance) {
     document.querySelector(".addProject").addEventListener('click', function() {
-        let projectForm = document.createElement('form')
-        projectForm.setAttribute('id', 'projectForm')
-        projectForm.method = 'GET'
-        let label = document.createElement('label')
-        label.setAttribute('for', 'projectName')
-        label.textContent = `Your project's title`
-        let input = document.createElement('input')
-        input.setAttribute('type', 'text')
-        input.setAttribute('id', 'projectName')
-        let button = document.createElement('button')
-        button.type = 'button'
-        button.textContent = 'Create'
-        button.classList.add('createProject')
+        if (!document.querySelector('#projectForm')) {
+            let projectForm = document.createElement('form')
+            projectForm.setAttribute('id', 'projectForm')
+            projectForm.method = 'GET'
+            let label = document.createElement('label')
+            label.setAttribute('for', 'projectName')
+            label.textContent = `Your project's title`
+            let input = document.createElement('input')
+            input.setAttribute('type', 'text')
+            input.setAttribute('id', 'projectName')
+            let button = document.createElement('button')
+            button.type = 'button'
+            button.textContent = 'Create'
+            button.classList.add('createProject')
 
-        projectForm.appendChild(label)
-        projectForm.appendChild(input)
-        projectForm.appendChild(button)
+            projectForm.appendChild(label)
+            projectForm.appendChild(input)
+            projectForm.appendChild(button)
 
-        document.querySelector('#sidebar').appendChild(projectForm)
+            document.querySelector('#sidebar').appendChild(projectForm)
 
-        button.addEventListener('click', function() {
-            if (document.querySelector('#projectName').value) {
-                let projectName = document.querySelector('#projectName').value.toString()
-                addProject(projectName, instance)
-                projectForm.remove();
-            }
-        })
+            button.addEventListener('click', function() {
+                if (document.querySelector('#projectName').value) {
+                    let projectName = document.querySelector('#projectName').value.toString()
+                    addProject(projectName, instance)
+                    projectForm.remove();
+                }
+            })
+        }
     })
 }
 
